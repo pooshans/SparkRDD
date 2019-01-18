797cd9c1a3f743017b71988aa77f153f24bc599e
pooshan_tala <t-pooshan@talasecurity.io>
2019-01-18 18:47:22 +0530

    Adding customer Spark RDD

---
+++ src/main/java/spark/extend/DiscountRDD.scala
@@ -0,0 +1,17 @@
+package spark.extend
+
+import org.apache.spark.{Partition, TaskContext}
+import org.apache.spark.rdd.RDD
+
+/**
+  * @author PooshanSingh
+  */
+class DiscountRDD(prev:RDD[SalesRecord],discountPercentage:Double) extends RDD[SalesRecord](prev){
+  override def compute(split: Partition, context: TaskContext): Iterator[SalesRecord] =  {
+    firstParent[SalesRecord].iterator(split, context).map(salesRecord => {
+      val discount = salesRecord.itemValue*discountPercentage
+      new SalesRecord(salesRecord.transactionId,salesRecord.customerId,salesRecord.itemId,discount)
+    })}
+
+  override protected def getPartitions: Array[Partition] = firstParent[SalesRecord].partitions
+}
---
+++ src/main/java/spark/extend/ExtendExample.scala
@@ -0,0 +1,28 @@
+package spark.extend
+
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkContext._
+import CustomFunctions._
+
+/**
+  * @author PooshanSingh
+  */
+object ExtendExample {
+  def main(args: Array[String]) {
+
+    val sc = new SparkContext("local[*]", "extendingspark")
+    val dataRDD = sc.textFile("/Users/pooshans/research/code/blog/SparkRDD/src/main/resources/sales.csv")
+    val salesRecordRDD = dataRDD.map(row => {
+      val colValues = row.split(",")
+      new SalesRecord(colValues(0),colValues(1),colValues(2),colValues(3).toDouble)
+    })
+
+    salesRecordRDD.map(_.itemValue).sum
+
+    println(salesRecordRDD.totalSales)
+
+    // discount RDD
+    val discountRDD = salesRecordRDD.discount(0.1)
+    println(discountRDD.collect().toList)
+  }
+}
