e9410dfa2e9277cab7e60fc0bb141057e6e398bf
pooshan_tala <t-pooshan@talasecurity.io>
2019-01-27 18:28:10 +0530

    Java custome rdd.

--- src/main/java/spark/extend/java/DiscountRDD.java
+++ src/main/java/spark/extend/java/DiscountRDD.java
@@ -4,12 +4,18 @@ import org.apache.spark.Partition;
 import org.apache.spark.SparkContext;
 import org.apache.spark.TaskContext;
 import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.rdd.RDD;
 import scala.collection.AbstractIterator;
 import scala.collection.Iterator;
+import scala.collection.mutable.ArrayBuffer;
 import scala.reflect.ClassManifestFactory$;
 import scala.reflect.ClassTag;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -21,46 +27,89 @@ public class DiscountRDD extends RDD<SalesRecord> {
     private RDD<SalesRecord> prev;
     private static final ClassTag<SalesRecord> SALES_RECORD_CLASS_TAG_TAG = ClassManifestFactory$.MODULE$.fromClass(SalesRecord.class);
     private JavaRDD<SalesRecord> salesRecordRDD;
+    private String dataSource;
 
     private SparkContext sc;
     public DiscountRDD(JavaRDD<SalesRecord> salesRecordRDD, double discountPercentage) {
         super(salesRecordRDD.rdd(),SALES_RECORD_CLASS_TAG_TAG);
-        //super(sc.sc(),new ArrayBuffer<>(), SALES_RECORD_CLASS_TAG_TAG);
         this.discountPercentage = discountPercentage;
         this.salesRecordRDD = salesRecordRDD;
+    }
+
+    public DiscountRDD(JavaSparkContext sc, String dataSource, double discountPercentage) {
+        super(sc.sc(),new ArrayBuffer<>(),SALES_RECORD_CLASS_TAG_TAG);
+        this.discountPercentage = discountPercentage;
+        this.salesRecordRDD = salesRecordRDD;
+        this.dataSource = dataSource;
     }
 
 
     @Override
     public Iterator<SalesRecord> compute(Partition split, TaskContext context) {
-        System.out.println("Split : " + split.index() + " , partition id : " + context.partitionId());
         DiscountPartition  discountPartition = (DiscountPartition)split;
-        return new DiscountIterator(discountPartition.salesRecord);
+        return new DiscountIterator(discountPartition);
     }
 
     public Partition[] getPartitions() {
-        List<SalesRecord> salesRecords =  this.salesRecordRDD.collect();
-        DiscountPartition[] discountPartitions = new DiscountPartition[salesRecords.size()];
-
+        List<DiscountPartition> discountPartitions = new ArrayList<DiscountPartition>();
         int index = 0;
-        java.util.Iterator<SalesRecord> itr=  salesRecords.iterator();
-        while(itr.hasNext()){
-            discountPartitions[index] = new DiscountPartition(index,itr.next());
-            index++;
-        }
-       return discountPartitions;
+        BufferedReader objReader = null;
+        try {
+            String strCurrentLine;
 
+            objReader = new BufferedReader(new FileReader(dataSource));
+            List<SalesRecord> salesRecords = null;
+            int rowCount = 0;
+
+            while ((strCurrentLine = objReader.readLine()) != null) {
+                if(salesRecords == null){
+                    salesRecords = new ArrayList<>();
+                }
+
+
+                String[] colValues = strCurrentLine.split(",");
+                SalesRecord salesRecord =  new SalesRecord(colValues[0], colValues[1], colValues[2], Double.valueOf(colValues[3]));
+                salesRecords.add(salesRecord);
+                rowCount++;
+                if(rowCount%2 == 0){
+                    //Each partition will have two row.
+                    DiscountPartition discountPartition = new DiscountPartition(index,salesRecords.toArray(new SalesRecord[]{}));
+                    discountPartitions.add(discountPartition);
+                    index++;
+                    salesRecords = null;
+                }
+            }
+            return discountPartitions.toArray(new DiscountPartition[]{});
+
+        } catch (IOException e) {
+
+            e.printStackTrace();
+
+        } finally {
+
+            try {
+                if (objReader != null)
+                    objReader.close();
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }
+
+return null;
     }
 
 
+    /**
+     * Each partition definition
+     **/
     public static class DiscountPartition implements Partition {
         private static final long serialVersionUID = 1L;
         private int index;
-        private SalesRecord salesRecord;
+        private SalesRecord[] salesRecords;
 
-        public DiscountPartition(int index,SalesRecord salesRecord) {
+        public DiscountPartition(int index,SalesRecord[] salesRecords) {
             this.index = index;
-            this.salesRecord = salesRecord;
+            this.salesRecords = salesRecords;
         }
 
         @Override
@@ -83,28 +132,28 @@ public class DiscountRDD extends RDD<SalesRecord> {
     }
 
     /**
-     * Iterators over all characters between two characters
+     * Iterators over all SalesRecords
      */
     public static class DiscountIterator extends AbstractIterator<SalesRecord> {
-        private SalesRecord salesRecord;
-        private boolean isAvailble = true;
+        private SalesRecord[] salesRecords;
+        private int rowIndex = 0;
 
 
-        public DiscountIterator(SalesRecord salesRecord) {
-            this.salesRecord = salesRecord;
+        public DiscountIterator(DiscountPartition discountPartition) {
+            this.salesRecords = discountPartition.salesRecords;
 
         }
 
         @Override
         public boolean hasNext() {
-            return isAvailble;
+            return rowIndex < salesRecords.length;
         }
 
         @Override
         public SalesRecord next() {
-            double discount = salesRecord.getItemValue()*discountPercentage;
-            SalesRecord newSalesRecord =  new SalesRecord(salesRecord.getTransactionId(),salesRecord.getCustomerId(),salesRecord.getItemId(),discount);
-            isAvailble = false;
+            double discount = salesRecords[rowIndex].getItemValue()*discountPercentage;
+            SalesRecord newSalesRecord =  new SalesRecord(salesRecords[rowIndex].getTransactionId(),salesRecords[rowIndex].getCustomerId(),salesRecords[rowIndex].getItemId(),discount);
+            rowIndex++;
             return  newSalesRecord;
 
         }
--- src/main/java/spark/extend/java/SparkExtendExample.java
+++ src/main/java/spark/extend/java/SparkExtendExample.java
@@ -2,26 +2,28 @@ package spark.extend.java;
 
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.api.java.function.DoubleFunction;
 
 /**
  * @author PooshanSingh
  */
 public class SparkExtendExample {
     public static void main(String[] args) {
+        String dataSource = "/Users/pooshans/research/code/blog/SparkRDD/src/main/resources/sales.csv";
 
         JavaSparkContext sc = new JavaSparkContext("local[*]", "extendingspark");
-        JavaRDD<String> dataRdd = sc.textFile("/Users/pooshans/research/code/blog/SparkRDD/src/main/resources/sales.csv",
+        JavaRDD<String> dataRdd = sc.textFile(dataSource,
                 5);
         JavaRDD<SalesRecord> salesRecordRDD = dataRdd.map(row -> {
             String[] colValues = row.split(",");
             return new SalesRecord(colValues[0], colValues[1], colValues[2], Double.valueOf(colValues[3]));
         });
 
-        //double totalSales = salesRecordRDD.mapToDouble((DoubleFunction<SalesRecord>) salesRecord -> salesRecord.getItemValue()).sum();
+        double totalSales = salesRecordRDD.mapToDouble((DoubleFunction<SalesRecord>) salesRecord -> salesRecord.getItemValue()).sum();
 
-        //System.out.println("Total Sales : "+totalSales);
+        System.out.println("Total Sales : "+totalSales);
 
-        DiscountRDD discountRDD = new DiscountRDD(salesRecordRDD, 0.1);
+        DiscountRDD discountRDD = new DiscountRDD(sc, dataSource,0.1);
 
         System.out.println("Total count : " + discountRDD.toJavaRDD().collect().size());
 
